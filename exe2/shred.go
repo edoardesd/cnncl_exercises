2a5a0b3bf7bcedb83c7808c70e1f5ce292dc3d48
edoardesd <edoardesd@gmail.com>
2022-11-23 16:50:04 +0100

    compact shred in a single function

--- exe2/shred.go
+++ exe2/shred.go
@@ -7,46 +7,27 @@ import (
     "time"
 )
 
-func shredIter(filename string) error{
-    const MAX_BYTES = 2000000
-
-    randomInt := rand.Intn(MAX_BYTES)
-    fmt.Println("Random length: ", randomInt)
-    randomBytes := make([]byte, randomInt)
-    rand.Read(randomBytes)
-
-    e := os.WriteFile(filename, randomBytes, 0644)
-    return e
-
-}
-
-func deleteFile(filename string) error {
-    err := os.Remove(filename)
-    if err != nil {
-        return err
-        }
-    return nil
-}
-
 func shred(filename string) (bool, error) {
     const N_SHRED = 3
+    const MAX_BYTES = 20000
 
     for i := 0; i <N_SHRED; i++ {
         fmt.Println("Iteration ", i+1, " of the shred function.")
+        randomInt := rand.Intn(MAX_BYTES)
+        fmt.Println("Random length: ", randomInt)
+        randomBytes := make([]byte, randomInt)
+        rand.Read(randomBytes)
 
-        err := shredIter(filename)
-
+        err := os.WriteFile(filename, randomBytes, 0644)
         if err != nil {
             return false, err
         }
-
     }
 
-
-    err := deleteFile(filename)
+    err := os.Remove(filename)
     if err != nil {
-            return false, err
-        }
+        return false, err
+    }
 
     return true, nil
 }

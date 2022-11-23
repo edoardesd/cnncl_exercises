2889c5af446553e0d1bb5b0bb3b5af2715644868
edoardesd <edoardesd@gmail.com>
2022-11-23 16:38:42 +0100

    add helper func shredIter

--- exe2/shred.go
+++ exe2/shred.go
@@ -7,20 +7,32 @@ import (
     "time"
 )
 
-const MAX_BYTES = 2000000
+func shredIter(filename string) error{
+    const MAX_BYTES = 2000000
 
-func shred(filename string) (bool, error) {
     randomInt := rand.Intn(MAX_BYTES)
     fmt.Println("Random length: ", randomInt)
     randomBytes := make([]byte, randomInt)
     rand.Read(randomBytes)
 
-    err := os.WriteFile(filename, randomBytes, 0644)
+    e := os.WriteFile(filename, randomBytes, 0644)
+    return e
 
-    if err!= nil{
-        return false, err
+}
+
+func shred(filename string) (bool, error) {
+    const N_SHRED = 3
+
+    for i := 0; i <N_SHRED; i++ {
+        fmt.Println("Iteration ", i+1, " of the shred function.")
+
+        err := shredIter(filename)
+
+        if err != nil {
+            return false, nil
+        }
+
     }
-
     return true, nil
 }
 
